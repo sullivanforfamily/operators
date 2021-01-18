f2d6686c045da1e838975989ae8c28ae9285fc2f
charlessullivan <sullivanforfamily@gmail.com>
2021-01-17 16:16:34 -0800

    commit tree 8ddedc0d0935710771cf123aae15f88f95602303

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
+
 def unsafe?(speed)
-
+    if speed < 40 || speed > 60 
+        puts "True"
+    else puts "False"
 end
-
 
 
 def not_safe?(speed)
-	
+    speed < 40 || speed > 60 ? "True" : "False"
 end
+
+
 	
 
 
